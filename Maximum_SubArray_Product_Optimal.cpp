edf7d9af2a1c0dfc8a73b5397f2d2bab721ea26d
kani-s-h-k <kanishk.malik.dev@gmail.com>
2025-12-30 16:33:35 +0530

    updated

---
+++ Maximum_SubArray_Product_Optimal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+    int n = nums.size();
+    int maxi = INT_MIN;
+	int prefix=1,suffix=1;
+	for(int i =0;i<n;i++){
+	    if(prefix==0) prefix=1;
+	    if(suffix ==0)suffix=1;
+	    prefix*=nums[i];
+	    suffix*=nums[n-i-1];
+	    maxi=max(maxi,max(prefix,suffix));
+	}
+    return maxi;
+    }
+};
