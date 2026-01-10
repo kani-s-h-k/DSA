fd905e5dd2b616020a000cd4d68510429f7b5ab6
kani-s-h-k <kanishk.malik.dev@gmail.com>
2026-01-10 17:11:17 +0530

    updated

---
+++ Linked List/Reverse_LL_Optimal_(Iterative).cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    
+    Node(int data1){
+        data = data1;
+    }
+    
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+Node* createLL(vector<int> &nums){
+    Node* head = new Node(nums[0]);
+    Node* prev = head;
+    for(int i=1;i<nums.size();i++){
+        Node* temp = new Node(nums[i]);
+        prev->next = temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void traverseLL(Node* head){
+    while(head){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+Node* reverseLL(Node* head){
+   Node* temp = head;
+   Node* prev = nullptr, *front =nullptr;
+   while(temp){
+       front = temp->next;
+       temp->next = prev;
+       prev = temp;
+       temp=front;
+   }
+   return prev;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	vector<int> nums(n);
+	for(int i = 0;i<n;i++){
+	    cin>>nums[i];
+	}
+    Node* head = createLL(nums);
+    traverseLL(head);
+    traverseLL(reverseLL(head));
+    return 0;
+    
+}
