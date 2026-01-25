09de044a109a2ce4aa406a6129a5979482760e5a
kani-s-h-k <kanishk.malik.dev@gmail.com>
2026-01-25 18:24:39 +0530

    updated

---
+++ Stack & Queue/PostfixToPrefix.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string postfixToInfix(string s){
+    int i =0;
+    int n = s.length();
+    stack<string> st;
+    
+    while(i<n){
+        if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || s[i]>='0' && s[i]<='9') st.push(string(1,s[i]));
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string con = s[i]+t2+t1;
+            st.push(con);
+        }
+        i++;
+    }
+    return st.top();
+}
+
+int main() {
+	string s;
+	cin>>s;
+	string res = postfixToInfix(s);
+	cout<<res<<" ";
+    return 0;
+}
