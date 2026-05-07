964f53b182ffe5be47c4546c39bcd29337f17071
kani-s-h-k <kanishk.malik.dev@gmail.com>
2026-05-07 18:59:43 +0530

    updated

---
+++ Binary Search/Find_Sqrt_Of_a_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int floorSqrt(int n)  {
+      int ans = 1,low =1, high = n;
+      while(low<=high){
+        int mid = low + (high-low)/2;
+        if((long long)mid*mid <= n){
+            ans = mid;
+            low = mid+1;
+        }
+        else high = mid - 1;
+      }
+      return ans;
+    }
+};
