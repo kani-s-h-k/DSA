ebca8fe90c7dce446e1d2758f5317673b7cfda1c
kani-s-h-k <kanishk.malik.dev@gmail.com>
2026-07-04 13:00:55 +0530

    updated

---
+++ Sliding Window & Two Pointers/Max_Points_U_Can_Obtain_From_Cards.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int lsum =0 ,rsum=0,r=cardPoints.size()-1,maxSum=0;
+        for(int i = 0;i<k;i++) lsum += cardPoints[i];
+        maxSum=lsum;
+        for(int i = k-1;i>=0;i--){
+            lsum -= cardPoints[i];
+            rsum += cardPoints[r];
+            r--;
+
+            maxSum = max(maxSum,lsum+rsum);
+        }
+        return maxSum;
+    }
+};
