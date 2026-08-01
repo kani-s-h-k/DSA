ca330704d591ea525b8bf5f4d6dd0e726d2ae81b
kani-s-h-k <kanishk.malik.dev@gmail.com>
2026-08-01 14:46:48 +0530

    updated

---
+++ DP/Min_Ins_To_Make_Str_Palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        string r(s.rbegin(),s.rend());
+        vector<int> prev(n+1,0),curr(n+1,0);
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                if(s[i-1]==r[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j],curr[j-1]);
+            }
+            prev = curr;
+        }
+        return n-prev[n];
+    }
+};
